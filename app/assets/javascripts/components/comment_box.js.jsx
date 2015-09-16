262b08e96501fc170f1e5cb9fa1e69cbee85bf07
Arturo Perez <VrturoPerez@gmail.com>
2015-09-15 21:43:55 -0700

    Add commentBox

---
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -0,0 +1,16 @@
+var CommentBox = React.createClass({
+  getInitialState: function(){
+    return{
+      <div />,
+    }
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+React.render(
+  <CommentBox />)
